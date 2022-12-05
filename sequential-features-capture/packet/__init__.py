7007703fc259b117d0de1627bf451e5dac8b94ed
David Puerta Martín <puertadavid96182@gmail.com>
2022-12-05 00:37:36 -0500

    update sequetial feat capture

--- sequential-features-capture/packet/__init__.py
+++ sequential-features-capture/packet/__init__.py
@@ -6,6 +6,8 @@ from utils import get_date_string
 INTERVAL = 15
 # TCP flags order for network state
 FLAGS_ORDER = 'FSRPAECU'
+ICMP_STATES = {'0': 'ECR', '1': 'UNK', '2': 'UNK', '3': 'URH', '5': 'RED', '7': 'URP', '8': 'ECO', '9': 'RTA',
+                           '10': 'RTS', '11': 'TXD', '12': 'PAR', '13': 'TST', '14': 'TSR', '40': 'PHO'}
 
 
 class FlowAnalysis:
@@ -30,15 +32,16 @@ class FlowAnalysis:
         self.duration = 0
 
         pkt_protocol = packet.highest_layer
-        if pkt_protocol == 'DATA':
-            pkt_protocol = packet.layers[len(packet.layers) - 2].layer_name
+        if 'TCP' in packet:
+            pkt_protocol = 'TCP'
+        elif 'UDP' in packet:
+            pkt_protocol = 'UDP'
         self.protocol = pkt_protocol
-
+        self.src_port = ''
+        self.dst_port = ''
         if self.protocol == 'ARP':
             self.src_adr = packet.arp.src_proto_ipv4
             self.dst_adr = packet.arp.dst_proto_ipv4
-            self.src_port = ''
-            self.dst_port = ''
         elif 'IP' in packet:
             self.src_adr = packet.ip.src
             self.dst_adr = packet.ip.dst
@@ -51,8 +54,7 @@ class FlowAnalysis:
             self.src_port = packet.tcp.srcport
             self.dst_port = packet.tcp.dstport
         elif 'ICMP' in packet:
-            self.src_port = packet.icmp.udp_srcport
-            self.dst_port = packet.icmp.udp_dstport
+            self.src_port = packet.icmp.checksum
         elif 'UDP' in packet:
             self.src_port = packet.udp.srcport
             self.dst_port = packet.udp.dstport
@@ -60,7 +62,7 @@ class FlowAnalysis:
         self.state = ''
         self.state = self.calculate_network_state(packet)
         self.d_tos = ''
-        if not hasattr(self, 's_stos'):
+        if not hasattr(self, 's_tos'):
             self.s_tos = ''
 
         self.tot_pkts = 1
@@ -68,12 +70,12 @@ class FlowAnalysis:
         self.src_bytes = int(packet.length)
 
         self.flow = 'Background'
-        if '172.22.0' in self.src_adr or '172.22.0' in self.dst_adr:
+        if '172.18.0' in self.src_adr or '172.18.0' in self.dst_adr:
             last_src = None
             last_dst = None
-            if '172.22.0' in self.src_adr:
+            if '172.18.0' in self.src_adr:
                 last_src = int(self.src_adr.split('.')[len(self.src_adr.split('.')) - 1])
-            if '172.22.0' in self.dst_adr:
+            if '172.18.0' in self.dst_adr:
                 last_dst = int(self.dst_adr.split('.')[len(self.dst_adr.split('.')) - 1])
             if (last_src and last_src > 3) or (last_dst and last_dst > 3):
                 self.flow = 'Botnet'
@@ -121,7 +123,9 @@ class FlowAnalysis:
         """
         state = ''
         if 'UDP' in packet:
-            self.state = 'CON'
+            state = 'CON'
+            if 'UDP' != packet.highest_layer:
+                state = 'INT'
         if 'TCP' in packet:
             is_src = False
             if self.src_adr == packet.ip.src:
@@ -160,12 +164,15 @@ class FlowAnalysis:
                 state = ''.join([state_split[0], '_', state_update])
 
         if self.protocol == 'ARP':
-            if packet.arp.opcode == 1:
-                self.state = 'CON'
-            elif packet.arp.opcode == 2:
-                self.state = 'RSP'
-            else:
-                self.state = 'INT'
+            if packet.arp.opcode == '1':
+                state = 'CON'
+            elif packet.arp.opcode == '2':
+                state = 'RSP'
+        elif self.protocol == 'IGMP':
+            state = 'INT'
+        elif self.protocol == 'ICMP':
+            state = ICMP_STATES[packet.icmp.type]
+
         return state
 
     def update_elapsed_time(self):
--- sequential-features-capture/utils/__init__.py
+++ sequential-features-capture/utils/__init__.py
@@ -37,39 +37,41 @@ def get_flow_id(packet):
     @return: key and inverted key of the netflow
     """
     pkt_protocol = packet.highest_layer
-    if pkt_protocol == 'DATA':
-        pkt_protocol = packet.layers[len(packet.layers) - 2].layer_name
+    if 'TCP' in packet:
+        pkt_protocol = 'TCP'
+    elif 'UDP' in packet:
+        pkt_protocol = 'UDP'
+
+    src_port = ''
+    dst_port = ''
+    if pkt_protocol == 'ARP':
+        src_ip = packet.arp.src_proto_ipv4
+        dst_ip = packet.arp.dst_proto_ipv4
+
+        key = '%s;%s;%s' % (src_ip, dst_ip, pkt_protocol)
+        inv_key = '%s;%s;%s' % (dst_ip, src_ip, pkt_protocol)
+    else:
+        if 'IP' in packet:
+            src_ip = packet.ip.src
+            dst_ip = packet.ip.dst
+        elif 'IPV6' in packet:
+            src_ip = packet.ipv6.src
+            dst_ip = packet.ipv6.dst
+
+        if 'TCP' in packet:
+            src_port = packet.tcp.srcport
+            dst_port = packet.tcp.dstport
+        elif 'ICMP' in packet:
+            src_port = packet.icmp.checksum
+        elif 'UDP' in packet:
+            src_port = packet.udp.srcport
+            dst_port = packet.udp.dstport
     try:
-        if pkt_protocol == 'ARP':
-            src_ip = packet.arp.src_proto_ipv4
-            dst_ip = packet.arp.dst_proto_ipv4
-
-            key = '%s;%s;%s' % (src_ip, dst_ip, pkt_protocol)
-            inv_key = '%s;%s;%s' % (dst_ip, src_ip, pkt_protocol)
-        else:
-            if 'IP' in packet:
-                src_ip = packet.ip.src
-                dst_ip = packet.ip.dst
-            elif 'IPv6' in packet:
-                src_ip = packet.ipv6.src
-                dst_ip = packet.ipv6.dst
-
-            if 'TCP' in packet:
-                src_port = packet.tcp.srcport
-                dst_port = packet.tcp.dstport
-            elif 'ICMP' in packet:
-                src_port = packet.icmp.udp_srcport
-                dst_port = packet.icmp.udp_dstport
-            else:
-                src_port = packet.udp.srcport
-                dst_port = packet.udp.dstport
-
-            key = "%s; %s; -> %s; %s; %s" % (src_ip, src_port, dst_ip, dst_port, pkt_protocol)
-            inv_key = "%s; %s; -> %s; %s; %s" % (dst_ip, dst_port, src_ip, src_port, pkt_protocol)
-
-    except AttributeError():
-        logging.error(f'Packet has no IP layer: {packet.highest_layer}')
-        logging.error(packet)
-        logging.error(packet.ip.src)
+        key = "%s; %s; -> %s; %s; %s" % (src_ip, src_port, dst_ip, dst_port, pkt_protocol)
+        inv_key = "%s; %s; -> %s; %s; %s" % (dst_ip, dst_port, src_ip, src_port, pkt_protocol)
+    except Exception as e:
+        print(e)
+        print(packet.highest_layer)
+        print(packet.layers)
 
     return key, inv_key
